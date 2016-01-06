f4b3a0f0f2e6293427f54dea4d76cc771e830662
Minghe Huang <h.minghe@gmail.com>
2016-01-06 23:01:56 +0800

    add notification support

--- Applize/JavaScript/notification.js
+++ Applize/JavaScript/notification.js
@@ -1,97 +1,29 @@
-/*
-   notification.js
-   Applize
+'use strict';
 
-   Created by huangmh on 1/3/16.
-   Copyright © 2016 minghe. All rights reserved.
- */
-(function(window) {
+//
 
-  'use strict';
+var Notification = function(title, options) {
+  if (typeof title === 'undefined') {
+    title = ''; 
+  }   
+  window.location.href = "APPLIZE://ACTION/NOTIFICATION/DATA/" + title;
+};
 
-  var NativeNotification = window.Notification,
-    PrefixedNotification = window.webkitNotifications;
+Notification.requestPermission = function(callback) {
+  callback('granted');
+};
 
-  var utils = {
-    isFunction: function(obj) {
-      return typeof obj === 'function';
-    }
-  };
+Notification.permission = 'granted';
 
-  function WebNotification(title, options) {
-    console.log('created');
-    window.location.href = 'http://baidu.com';
-    if (!("Notification" in window)) {
-      alert(title);
-    } else {
-      return new NativeNotification(title, options);
-    }
-  }
+// length is readonly propery of function, 
+// and it's unconfigurable, so I have no 
+// idea to set it to 1, but Web Notification
+// 's lenght should be 1 at begining
+Object.defineProperty(Notification, "length", {
+    value: 0,
+    configurable: true,
+    writable: true,
+    enumerable: true
+});
 
-  WebNotification.getPermission = function() {
-
-    if (NativeNotification) {
-      // Official W3C/WHATWG Web Notifications API
-      if (NativeNotification.permission) {
-        return NativeNotification.permission;
-      }
-
-      // Older WebKit API
-      if (utils.isFunction(NativeNotification.permissionLevel)) {
-        return NativeNotification.permissionLevel();
-      }
-    }
-
-    // Oldprefixed WebKit API
-    if (PrefixedNotification && utils.isFunction(PrefixedNotification.checkPermission)) {
-      switch (PrefixedNotification.checkPermission()) {
-        case 0:
-          return 'granted';
-        case 1:
-          return 'default';
-        case 2:
-          return 'denied';
-      }
-    }
-
-    return 'default';
-  };
-
-  WebNotification.requestPermission = function(callback) {
-    var context = this;
-
-    function _onPermissionRequested() {
-      context.permission = context.getPermission();
-
-      if (callback) {
-        callback.call(context);
-      }
-    }
-
-    // Native first, then prefxied
-    if (NativeNotification && utils.isFunction(NativeNotification.requestPermission)) {
-      NativeNotification.requestPermission(_onPermissionRequested);
-    } else if (PrefixedNotification && utils.isFunction(PrefixedNotification.requestPermission)) {
-      PrefixedNotification.requestPermission(_onPermissionRequested);
-    } else {
-      throw 'Could not call requestPermission';
-    }
-
-  };
-
-  WebNotification.permission = WebNotification.getPermission();
-
-  if (typeof define === 'function' && define.amd) {
-    define(function() {
-      if ('Notification' in window) {
-        window.Notification = WebNotification;
-        return WebNotification;
-      }
-    });
-  } else {
-    if ('Notification' in window) {
-      window.Notification = WebNotification;
-    }
-  }
-
-})(this);
+Notification.length = 1;
